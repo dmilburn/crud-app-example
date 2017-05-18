67170cdefac24e2b5b6cc7e910a08ee4f8466837
Debbie Milburn <debbiemilburn2@gmail.com>
2017-05-18 12:11:32 -0400

    Make clothing items save

--- app/controllers/clothing_items_controller.rb
+++ app/controllers/clothing_items_controller.rb
@@ -2,3 +2,13 @@ get '/clothing_items' do
   @clothing_items = ClothingItem.all
   erb :'clothing_items/index'
 end
+
+post '/clothing_items' do
+  clothing_item = ClothingItem.new(params[:clothing_item])
+  if clothing_item.save
+    redirect '/clothing_items'
+  else
+    @errors = clothing_item.errors.full_messages
+    erb :'/clothing_items/index'
+  end
+end
