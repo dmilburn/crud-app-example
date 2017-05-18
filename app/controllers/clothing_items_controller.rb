fa5ef8d15d20d150de2bc2552b41f1e680090ab1
Debbie Milburn <debbiemilburn2@gmail.com>
2017-05-18 14:50:29 -0400

    Fix bugsgit add .

--- app/controllers/clothing_items_controller.rb
+++ app/controllers/clothing_items_controller.rb
@@ -7,7 +7,7 @@ post '/clothing_items' do
   clothing_item = ClothingItem.new(params[:clothing_item])
   if clothing_item.save
     if request.xhr?
-      erb :'/clothing_items/single'
+      erb :'/clothing_items/_single', locals: {clothing_item: clothing_item}, layout: false
     else
       redirect '/clothing_items'
     end
--- public/js/application.js
+++ public/js/application.js
@@ -1,15 +1,17 @@
 $(document).ready(function() {
 
-	$('#new-clothing-item-form').on('click', function(event){
-		var url = $(this).attr('method');
-		var method = $(this).attr('action');
+	$('#new-clothing-item-form').on('submit', function(event){
+		event.preventDefault();
+		var url = $(this).attr('action');
+		var method = $(this).attr('method');
 		var data = $(this).serialize();
 		$.ajax({
 			url:url,
 			method: method,
 			data:data
 		}).done(function(response){
-			$('closet-container').append(repsonse)
+			debugger
+			$('#clothing-item-list').append(response)
 		})
 
 	})
